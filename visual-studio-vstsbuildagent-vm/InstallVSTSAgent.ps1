a4e4167942c5bf94b236099b9e3460df30829b52
MikeB <duk3ofed@users.noreply.github.com>
2018-11-28 15:28:59 +0000

    fixes old commit

--- visual-studio-vstsbuildagent-vm/InstallVSTSAgent.ps1
+++ visual-studio-vstsbuildagent-vm/InstallVSTSAgent.ps1
@@ -21,9 +21,16 @@ try {
   # installed (.NET 4.5 is an in-place upgrade).
   [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
 } catch {
-  Write-Warning 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to do one or more of the following: (1) upgrade to .NET Framework 4.5 and PowerShell v3, (2) specify internal Chocolatey package location (set $env:chocolateyDownloadUrl prior to install or host the package internally), (3) use the Download + PowerShell method of install. See https://chocolatey.org/install for all install options.'
+  Write-Warning 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed.'
 }
 
+function InitialiseDataDrive {
+    Get-Disk |
+    Where-Object partitionstyle -eq 'raw' |
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -AssignDriveLetter -UseMaximumSize |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data" -Confirm:$false
+}
 function PrepMachineForAutologon () {
     # Create a PS session for the user to trigger the creation of the registry entries required for autologon
     $computerName = "localhost"
@@ -125,8 +132,16 @@ do {
 } 
 while ($retries -le $retryCount)
 
-# Identify correct destination drive letter for Agent installation files (including _work directory)
-$volume = "$(get-volume | where-object {($_.SizeRemaining -GE 350GB) -and ($_.driveletter -ne $($env:SystemDrive).trim(':'))} | select -first 1 -ExpandProperty DriveLetter):"
+InitialiseData
+
+# Identify correct destination drive letter for Agent installation.
+Try {
+    InitialiseDataDrive -ErrorAction Stop
+    $volume = "$(Get-Volume | Where-Object FileSystemLabel -EQ 'Data' | Select-Object -ExpandProperty DriveLetter):"
+}
+Catch {
+    $volume = $env:systemdrive
+}
 # Construct the agent folder under a volume .
 $agentInstallationPath = Join-Path $volume $AgentName 
 # Create the directory for this agent.
